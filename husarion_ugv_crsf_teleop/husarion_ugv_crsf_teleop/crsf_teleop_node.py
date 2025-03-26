5aa9ea1a4e7e4c1024ea21128efc7073664ffd32
kmakd <kmakd197@gmail.com>
2025-03-26 10:04:22 +0000

    add twist stamped

--- husarion_ugv_crsf_teleop/husarion_ugv_crsf_teleop/crsf_teleop_node.py
+++ husarion_ugv_crsf_teleop/husarion_ugv_crsf_teleop/crsf_teleop_node.py
@@ -17,6 +17,7 @@ from enum import IntEnum
 import rclpy
 import serial
 from geometry_msgs.msg import Twist
+from geometry_msgs.msg import TwistStamped
 from rcl_interfaces.msg import FloatingPointRange, ParameterDescriptor
 from rclpy.node import Node
 from rclpy.qos import QoSDurabilityPolicy, QoSProfile, QoSReliabilityPolicy
@@ -54,8 +55,30 @@ class CRSFInterface(Node):
     def __init__(self):
         super().__init__("crsf_interface")
 
-        self.cmd_vel_publisher = self.create_publisher(
-            Twist,
+        self._declare_node_parameters()
+
+        [
+            port,
+            baud,
+            self._cmd_vel_stamped,
+            e_stop_republish,
+            self._enable_cmd_vel_silence_switch,
+            self._linear_speed_presets,
+            self._angular_speed_presets,
+        ] = self.get_parameters(
+            [
+                "port",
+                "baud",
+                "cmd_vel_stamped",
+                "e_stop_republish",
+                "enable_cmd_vel_silence_switch",
+                "linear_speed_presets",
+                "angular_speed_presets",
+            ]
+        )
+
+        self._cmd_vel_publisher = self.create_publisher(
+            TwistStamped if self._cmd_vel_stamped.value else Twist,
             "cmd_vel",
             QoSProfile(
                 reliability=QoSReliabilityPolicy.RELIABLE,
@@ -64,7 +87,7 @@ class CRSFInterface(Node):
             ),
         )
 
-        self.link_status_publisher = self.create_publisher(
+        self._link_status_publisher = self.create_publisher(
             LinkStatus,
             "link_status",
             QoSProfile(
@@ -74,64 +97,52 @@ class CRSFInterface(Node):
             ),
         )
 
-        self.e_stop_trigger = self.create_client(
+        self._e_stop_trigger = self.create_client(
             Trigger,
             "hardware/e_stop_trigger",
         )
 
-        self.e_stop_reset = self.create_client(
+        self._e_stop_reset = self.create_client(
             Trigger,
             "hardware/e_stop_reset",
         )
 
-        self.link_status = LinkStatus()
+        self._link_status = LinkStatus()
 
-        self.declare_node_parameters()
-
-        [
-            port,
-            baud,
-            e_stop_republish,
-            self.enable_cmd_vel_silence_switch,
-            self.linear_speed_presets,
-            self.angular_speed_presets,
-        ] = self.get_parameters(
-            [
-                "port",
-                "baud",
-                "e_stop_republish",
-                "enable_cmd_vel_silence_switch",
-                "linear_speed_presets",
-                "angular_speed_presets",
-            ]
-        )
-
-        if len(self.linear_speed_presets.value) != 3 or len(self.angular_speed_presets.value) != 3:
+        if (
+            len(self._linear_speed_presets.value) != 3
+            or len(self._angular_speed_presets.value) != 3
+        ):
             raise ValueError("Speed presets must be a list of 3 values")
 
-        self.serial = None
-        while self.serial is None:
+        self._serial = None
+        while self._serial is None:
             try:
-                self.serial = serial.Serial(port.value, baud.value, timeout=2)
+                self._serial = serial.Serial(port.value, baud.value, timeout=2)
             except serial.SerialException as e:
                 self.get_logger().error(f"Failed to open serial port: {e}")
                 rclpy.spin_once(self, timeout_sec=2)
 
-        self.parser = CRSFParser()
-        self.parser.on_message = lambda msg: self.handle_message(msg)
+        self._parser = CRSFParser()
+        self._parser.on_message = lambda msg: self._handle_message(msg)
 
-        self.serial_parser_timer = self.create_timer(0.01, self.serial_parser_timer_cb)
+        self._serial_parser_timer = self.create_timer(0.01, self._serial_parser_timer_cb)
 
-        self.rc_estop_state = True
+        self._rc_estop_state = True
         if e_stop_republish.value:
-            self.e_stop_republisher = self.create_timer(1, self.update_e_stop)
+            self.e_stop_republisher = self.create_timer(1, self._update_e_stop)
 
-    def declare_node_parameters(self):
+    def _declare_node_parameters(self):
         self.declare_parameter(
             "port", "/dev/ttyUSB0", ParameterDescriptor(description="CRSF receiver serial port")
         )
         self.declare_parameter(
             "baud", 576000, ParameterDescriptor(description="CRSF receiver baud rate")
+        )
+        self.declare_parameter(
+            "cmd_vel_stamped",
+            False,
+            ParameterDescriptor(description="Publish cmd_vel as TwistStamped instead of Twist"),
         )
         self.declare_parameter(
             "e_stop_republish",
@@ -162,11 +173,11 @@ class CRSFInterface(Node):
             ),
         )
 
-    def serial_parser_timer_cb(self):
-        if self.serial.in_waiting > 0:
-            self.parser.parse(self.serial.read(self.serial.in_waiting))
+    def _serial_parser_timer_cb(self):
+        if self._serial.in_waiting > 0:
+            self._parser.parse(self._serial.read(self._serial.in_waiting))
 
-    def handle_message(self, msg: CRSFMessage):
+    def _handle_message(self, msg: CRSFMessage):
         if msg.msg_type == PacketType.RC_CHANNELS_PACKED:
             channels = unpack_channels(msg.payload)
             channels = normalize_channel_values(channels)
@@ -176,88 +187,97 @@ class CRSFInterface(Node):
             # if enabled by the 'e_stop_republish' parameter
             # Deasserted e-stop is transmitted only once
             requested_e_stop = channels[Switch.SF] < REQUESTED_E_STOP_THRESHOLD
-            if requested_e_stop != self.rc_estop_state:
-                self.rc_estop_state = requested_e_stop
+            if requested_e_stop != self._rc_estop_state:
+                self._rc_estop_state = requested_e_stop
 
-                if self.rc_estop_state:
-                    self.e_stop_trigger.call_async(Trigger.Request())
+                if self._rc_estop_state:
+                    self._e_stop_trigger.call_async(Trigger.Request())
                 else:
-                    self.e_stop_reset.call_async(Trigger.Request())
+                    self._e_stop_reset.call_async(Trigger.Request())
 
             # Disable sending cmd_vel if override switch is asserted
-            if self.enable_cmd_vel_silence_switch.value:
+            if self._enable_cmd_vel_silence_switch.value:
                 send_cmd_vel = channels[Switch.SA] < SEND_CMD_VEL_THRESHOLD
             else:
                 send_cmd_vel = True
 
             if send_cmd_vel:
-                linear_speed_modifier = self.linear_speed_presets.value[
+                linear_speed_modifier = self._linear_speed_presets.value[
                     round(channels[Switch.SG]) + 1
                 ]
-                angular_speed_modifier = self.angular_speed_presets.value[
+                angular_speed_modifier = self._angular_speed_presets.value[
                     round(channels[Switch.SG]) + 1
                 ]
 
-                t = Twist()
-                t.linear.x = channels[Switch.RIGHT_HORIZONTAL] * linear_speed_modifier
-                t.angular.z = -channels[Switch.LEFT_VERTICAL] * angular_speed_modifier
+                twist_msg = Twist()
+                twist_msg.linear.x = channels[Switch.RIGHT_HORIZONTAL] * linear_speed_modifier
+                twist_msg.angular.z = -channels[Switch.LEFT_VERTICAL] * angular_speed_modifier
 
-                if abs(t.linear.x) < PAD_DEADZONE:
-                    t.linear.x = 0.0
-                if abs(t.angular.z) < PAD_DEADZONE:
-                    t.angular.z = 0.0
+                if abs(twist_msg.linear.x) < PAD_DEADZONE:
+                    twist_msg.linear.x = 0.0
+                if abs(twist_msg.angular.z) < PAD_DEADZONE:
+                    twist_msg.angular.z = 0.0
 
-                self.cmd_vel_publisher.publish(t)
+                self._publish_twist(twist_msg)
 
         elif msg.msg_type == PacketType.LINK_STATISTICS:
-            last_lq = self.link_status.lq
+            last_lq = self._link_status.lq
 
-            self.link_status.header.stamp = self.get_clock().now().to_msg()
+            self._link_status.header.stamp = self.get_clock().now().to_msg()
 
-            self.link_status.rssi_1 = -msg.payload[0]
-            self.link_status.rssi_2 = -msg.payload[1]
-            self.link_status.lq = msg.payload[2]
-            self.link_status.uplink_snr = int.from_bytes(
+            self._link_status.rssi_1 = -msg.payload[0]
+            self._link_status.rssi_2 = -msg.payload[1]
+            self._link_status.lq = msg.payload[2]
+            self._link_status.uplink_snr = int.from_bytes(
                 bytes(msg.payload[3]), byteorder="big", signed=True
             )
-            self.link_status.used_antenna = msg.payload[4]
-            self.link_status.mode = msg.payload[5]
-            self.link_status.tx_power = msg.payload[6]
-            self.link_status.downlink_rssi = -msg.payload[7]
-            self.link_status.downlink_lq = msg.payload[8]
-            self.link_status.downlink_snr = int.from_bytes(
+            self._link_status.used_antenna = msg.payload[4]
+            self._link_status.mode = msg.payload[5]
+            self._link_status.tx_power = msg.payload[6]
+            self._link_status.downlink_rssi = -msg.payload[7]
+            self._link_status.downlink_lq = msg.payload[8]
+            self._link_status.downlink_snr = int.from_bytes(
                 bytes(msg.payload[9]), byteorder="big", signed=True
             )
 
             # Detect connection status
-            if last_lq != 0 and self.link_status.lq == 0:
+            if last_lq != 0 and self._link_status.lq == 0:
                 self.get_logger().error("Connection lost")
 
                 # Publish empty cmd_vel to stop the robot
-                self.cmd_vel_publisher.publish(Twist())
+                self._publish_twist(Twist())
 
-            if last_lq == 0 and self.link_status.lq > 0:
+            if last_lq == 0 and self._link_status.lq > 0:
                 self.get_logger().info("Connected")
 
             # Warn on low link quality
-            if last_lq != self.link_status.lq:
-                if self.link_status.lq < LINK_QUALITY_VERY_LOW_THRESHOLD:
-                    self.get_logger().warn(f"Very low link quality: {self.link_status.lq}%")
-                elif self.link_status.lq < LINK_QUALITY_LOW_THRESHOLD:
-                    self.get_logger().warn(f"Low link quality: {self.link_status.lq}%")
-                elif last_lq < 30 and self.link_status.lq >= LINK_QUALITY_LOW_THRESHOLD:
-                    self.get_logger().info(f"Link quality restored: {self.link_status.lq}%")
+            if last_lq != self._link_status.lq:
+                if self._link_status.lq < LINK_QUALITY_VERY_LOW_THRESHOLD:
+                    self.get_logger().warn(f"Very low link quality: {self._link_status.lq}%")
+                elif self._link_status.lq < LINK_QUALITY_LOW_THRESHOLD:
+                    self.get_logger().warn(f"Low link quality: {self._link_status.lq}%")
+                elif last_lq < 30 and self._link_status.lq >= LINK_QUALITY_LOW_THRESHOLD:
+                    self.get_logger().info(f"Link quality restored: {self._link_status.lq}%")
 
-            self.link_status_publisher.publish(self.link_status)
+            self._link_status_publisher.publish(self._link_status)
 
         else:
             self.get_logger().warn(
                 f"Unknown CRSF message (Type: {msg.msg_type.name}, Length: {msg.length})"
             )
 
-    def update_e_stop(self):
-        if self.rc_estop_state:
-            self.e_stop_trigger.call_async(Trigger.Request())
+    def _update_e_stop(self):
+        if self._rc_estop_state:
+            self._e_stop_trigger.call_async(Trigger.Request())
+
+    def _publish_twist(self, twist: Twist):
+        if self._cmd_vel_stamped.value:
+            twist_stamped_msg = TwistStamped()
+            twist_stamped_msg.header.stamp = self.get_clock().now().to_msg()
+            twist_stamped_msg.twist = twist
+            self._cmd_vel_publisher.publish(twist_stamped_msg)
+        else:
+            self._cmd_vel_publisher.publish(twist)
 
 
 def main(args=None):
