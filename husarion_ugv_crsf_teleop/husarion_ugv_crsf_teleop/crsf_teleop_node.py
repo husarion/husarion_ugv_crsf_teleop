bf83aea684e46d5b5c5742458050a4c701f5b086
Jakub Delicat <jakub.delicat@husarion.com>
2025-12-17 15:04:39 +0100

    Revert "Merge pull request #11 from husarion/fix-lost-connection-cmd-vel"
    This reverts commit f4fc71606c9a3db19fb823e3993358320ae5fdae, reversing
    changes made to db3badb1e7d178f11fc5e093eddd2c303856abd0.

--- husarion_ugv_crsf_teleop/husarion_ugv_crsf_teleop/crsf_teleop_node.py
+++ husarion_ugv_crsf_teleop/husarion_ugv_crsf_teleop/crsf_teleop_node.py
@@ -251,14 +251,7 @@ class CRSFInterface(Node):
                 self.get_logger().error("Connection lost")
 
                 # Publish empty cmd_vel to stop the robot
-                empty_twist = Twist()
-                if self._cmd_vel_stamped.value:
-                    twist_stamped_msg = TwistStamped()
-                    twist_stamped_msg.header.stamp = self.get_clock().now().to_msg()
-                    twist_stamped_msg.twist = empty_twist
-                    self._cmd_vel_publisher.publish(twist_stamped_msg)
-                else:
-                    self._cmd_vel_publisher.publish(empty_twist)
+                self._publish_twist(Twist())
 
             if last_lq == 0 and self._link_status.lq > 0:
                 self.get_logger().info("Connected")
